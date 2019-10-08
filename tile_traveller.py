0c0af7a5e4d90315047fd14b13e0c2718e8fb715
Helena Jónsdóttir <helenaj18@ru.is>
2019-10-08 10:04:18 +0000

    nr 3

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,3 +1,4 @@
+import random
 # Constants
 NORTH = 'n'
 EAST = 'e'
@@ -61,7 +62,9 @@ def find_directions(col, row):
 def pull_lever(col,row, coin_list):
 
     if col == 1 and row == 2 or col == 2 and row == 2 or col == 3 and row == 2 or col == 2 and row == 3:
-        pull_lever = input('Pull a lever (y/n): ').lower()
+        yes_no_seq = ['y','n']
+        pull_lever = random.choice(yes_no_seq)
+        print('Pull a lever (y/n):',pull_lever)
         if pull_lever == 'y':
             coin_list.append(1)
             coin_sum=sum(coin_list)
@@ -77,8 +80,9 @@ def play_one_move(col, row, valid_directions,coin_list):
     ''' Plays one move of the game
         Return if victory has been obtained and updated col,row '''
     victory = False
-    direction = input("Direction: ")
-    direction = direction.lower()
+    direction_seq = ['n','s','w','e']
+    direction = random.choice(direction_seq)
+    print('Direction:',direction)
     
     if not direction in valid_directions:
         print("Not a valid direction!")
@@ -88,13 +92,18 @@ def play_one_move(col, row, valid_directions,coin_list):
     return victory, col, row
 
 def play_again():
-    play_again = input('Play again (y/n): ').lower()
+    yes_no_seq = ['y','n']
+    play_again = random.choice(yes_no_seq)
+    print('Play again (y/n):', play_again)
+
     if play_again == 'y':
         victory = False
         coin_list = []
         col, row = 1,1
     else: 
         victory = True
+        coin_list = []
+        col,row = 1,1
     return victory, coin_list, col, row
 
     
@@ -105,6 +114,8 @@ row = 1
 col = 1
 coin_list=[]
 
+seed = input('Input seed: ')
+
 valid_directions = NORTH
 print_directions(valid_directions)
 
@@ -114,8 +125,9 @@ while not victory:
     if victory:
         print("Victory! Total coins {}.".format(coin_sum))
         victory, coin_list, col, row = play_again()
-        valid_directions = find_directions(col, row)
-        print_directions(valid_directions)
+        if not victory:
+            valid_directions = find_directions(col, row)
+            print_directions(valid_directions)
     else:
         valid_directions = find_directions(col, row)
         print_directions(valid_directions)
